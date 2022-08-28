00d975d2d2739d25935304f11a5cf5d3152264f0
Belian Tomasz <bel.tomasz@gmail.com>
2022-08-28 13:27:02 +0200

    test

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
+#Script ausführen mit: 
+#rm install.sh &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/install.sh &> /dev/null; bash install.sh
+
 echo Noch ein Test mit Github
