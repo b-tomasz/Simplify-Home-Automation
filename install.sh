f286b925b8fb9a08dd0c909d743138f9feeef570
Belian Tomasz <bel.tomasz@gmail.com>
2022-09-01 17:52:48 +0200

    1

--- install.sh
+++ install.sh
@@ -28,7 +28,21 @@ pwd
 ls -lisa
 
 
+DIALOG_RESULT=result
 
+# Generate the dialog box
+dialog --title "INPUT BOX" \
+  --clear  \
+  --inputbox \
+"Hi, this is an input dialog box. You can use \n
+this to ask questions that require the user \n
+to input a string as the answer. You can \n
+input strings of length longer than the \n
+width of the input box, in that case, the \n
+input field will be automatically scrolled. \n
+You can use BACKSPACE to correct errors. \n\n
+Try entering your name below:" \
+16 51 2> $tmp_file
 
 
 
