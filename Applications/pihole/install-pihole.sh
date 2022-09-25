0ec7c04d772860c2f8aaeadece999889db682398
Belian Tomasz <bel.tomasz@gmail.com>
2022-09-25 14:36:23 +0200

    fixed Commet

--- Applications/pihole/install-pihole.sh
+++ Applications/pihole/install-pihole.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Script ausführen mit:
-#rm install.sh &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/pihole/install-pihole.sh &> /dev/null; bash install-pihole.sh
+#rm install-pihole.sh &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/pihole/install-pihole.sh &> /dev/null; bash install-pihole.sh
 
 # create Applikations folder
 mkdir -p /var/homeautomation/pihole
