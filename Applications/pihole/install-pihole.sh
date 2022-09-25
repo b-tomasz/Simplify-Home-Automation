4ee538d76aff0c1d79155dc597de5ac9c57ba766
Belian Tomasz <bel.tomasz@gmail.com>
2022-09-25 14:32:03 +0200

    added install-pihole.sh

---
+++ Applications/pihole/install-pihole.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Script ausführen mit:
+#rm install.sh &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/pihole/install-pihole.sh &> /dev/null; bash install.sh
+
+# create Applikations folder
+mkdir -p /var/homeautomation/pihole
+
+
+# change to folder
+cd /var/homeautomation/pihole
+
+# downlod docker-compose.yml and run it
+rm docker-compose.yml &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/pihole/docker-compose.yml &> /dev/null
+
+docker-compose up -d
