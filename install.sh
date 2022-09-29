1bafaf99a6438dcf392647eaf10537cab21d8a9e
Belian Tomasz <bel.tomasz@gmail.com>
2022-09-29 21:55:21 +0200

    sdded check/installation for Docker

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@
 # Create Script/Log Folder
 mkdir -p /var/homeautomation/script/log
 cd /var/homeautomation/script
+echo "Script Started at: " $(date) > $LOG_PWD/install.log
 
 ### Variables
 
@@ -34,8 +35,7 @@ LOG_PWD=/var/homeautomation/script/log
 exit_script () {
     #Remove all Files used by the Script
     
-    # Remove Log Folder
-    rm -r $LOG_PWD
+    
     
     
     
@@ -44,15 +44,18 @@ exit_script () {
     
     # Remove the install Script and Exit
     rm $SCRIPT_PWD/$SCRIPT_NAME
-    exit 1
+    echo Exit Code: $1
+    exit $1
 }
 
 # Update the Systen
 update_system () {
     if (whiptail --title "Update System" --yesno "Do you want to Update Your System?" --yes-button "Update" --no-button "Skip" 8 78); then
         
+        echo "Update Started at: " $(date) > $LOG_PWD/update.log
+        
         {
-            printf "\n\n----------Update System----------\n" >> $LOG_PWD/update.log
+            printf "\n\n----------Update Packages----------\n" >> $LOG_PWD/update.log
             apt-get update  -y -q >> $LOG_PWD/update.log
             
             echo -e "XXX\n20\nUpgrade System...\nXXX"
@@ -67,10 +70,10 @@ update_system () {
             echo -e "XXX\n100\nFinished...\nXXX"
             sleep 0.5
             
-        } | whiptail --gauge "Update System..." 6 50 0
+        } | whiptail --gauge "Update Packages..." 6 50 0
         
     else
-        echo "Update of System Skipped by User" >> $LOG_PWD/install.log
+        echo "System update Skipped by User" >> $LOG_PWD/install.log
         return
     fi
     
@@ -79,21 +82,49 @@ update_system () {
 # Install Docker
 check_docker_installation () {
     
+    # Check if Docker is installed completly
     
-    
-    if (whiptail --title "Install Docker" --yesno "Docker is not installed yet. Do you want to install Docker now?" --yes-button "Install" --no-button "Exit" 8 78); then
-        printf "User selected Yes, exit status was $?."
+    if (dpkg -s docker.io docker-compose); then
+        # Docker is already installed
+        
+        echo "Docker is already installed" >> $LOG_PWD/install.log
+        whiptail --title "Docker Installation" --infobox "Docker ist already installed on this System" --ok-button "Continue" 8 78
+        
     else
-        printf "Install of Docker aborted"
-        exit_script
+        
+        if (whiptail --title "Install Docker" --yesno "Docker is not installed yet. Do you want to install Docker now?" --yes-button "Install" --no-button "Exit" 8 78); then
+            # Install Docker
+            
+            {
+                printf "\n\n----------Update Packages----------\n" >> $LOG_PWD/install.log
+                apt-get update  -y -q >> $LOG_PWD/install.log
+                
+                echo -e "XXX\n20\nInstall docker.io...\nXXX"
+                printf "\n\n----------Install docker.io----------\n" >> $LOG_PWD/install.log
+                apt-get install docker.io -y -q >> $LOG_PWD/install.log
+                
+                echo -e "XXX\n50\nInstall docker-compose...\nXXX"
+                printf "\n\n----------Install docker-compose----------\n" >> $LOG_PWD/install.log
+                apt-get install docker-compose -y -q >> $LOG_PWD/install.log
+                
+                echo -e "XXX\n80\nCleanup System...\nXXX"
+                printf "\n\n----------Cleanup System----------\n" >> $LOG_PWD/install.log
+                apt-get autoremove -y -q >> $LOG_PWD/install.log
+                apt-get clean -y -q >> $LOG_PWD/install.log
+                
+                echo -e "XXX\n100\nFinished...\nXXX"
+                sleep 0.5
+                
+            } | whiptail --gauge "Update Packages..." 6 50 0
+            
+        else
+            # Exit Script
+            echo "Install of Docker aborted by the User" >> $LOG_PWD/install.log
+            exit_script 2
+        fi
+        
     fi
     
-    printf Test: $?
-    
-    
-    # install docker
-    
-    apt install docker.io docker-compose
     
     
 }
@@ -132,4 +163,4 @@ update_system
 
 # Finished all without Error
 printf "\n\n\nScript finished Succesfuly\n\n"
-exit_script
+exit_script 0
