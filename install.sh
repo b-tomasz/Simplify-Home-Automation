ac2d3630ed04cc36d225b622d3eaace5eaa27242
Samuel Ramseyer <samuel.ramseyer@hotmail.com>
2022-10-01 14:51:10 +0200

    fixes

--- install.sh
+++ install.sh
@@ -190,7 +190,7 @@ check_ip (){
         FIXED_IP=$(whiptail --title "IP Address" --inputbox "Which IP you want to set as Fixed IP for your Raspberry Pi?" 8 78 3>&1 1>&2 2>&3)
         if [ $? = 0 ]; then
             
-            sed -z 's/#interface eth0\n#static ip_address=192.168.0.10/interface eth0\nstatic ip_address=$FIXED_IP/' /etc/dhcpcd.conf > /etc/dhcpcd.conf.edited
+            sed -z 's/#interface eth0\n#static ip_address=192.168.0.10/interface eth0\nstatic ip_address="$FIXED_IP"/' /etc/dhcpcd.conf > /etc/dhcpcd.conf.edited
             mv /etc/dhcpcd.conf.edited /etc/dhcpcd.conf
             
         else
