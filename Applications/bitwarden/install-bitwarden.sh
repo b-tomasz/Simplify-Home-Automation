a07de0036fec2fc02819d6da8ea51011ac9b7c5c
Samuel Ramseyer <samuel.ramseyer@hotmail.com>
2022-10-07 17:29:40 +0200

    fixes

--- Applications/bitwarden/install-bitwarden.sh
+++ Applications/bitwarden/install-bitwarden.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Script ausführen mit:
-#cd /tmp; rm install-vpn.sh &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/vpn/install-vpn.sh &> /dev/null; bash install-vpn.sh
+#cd /tmp; rm install-bitwarden.sh &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/bitwarden/install-bitwarden.sh &> /dev/null; bash install-bitwarden.sh
 
 # Vergleich Openvpn und Wireguard:
 # https://www.ipvanish.com/blog/wireguard-vs-openvpn/
