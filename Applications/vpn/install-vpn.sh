e9efd162ff4ce18135d7ed4c2733ef1ed0a78340
Belian Tomasz <bel.tomasz@gmail.com>
2022-10-08 16:04:58 +0200

    changed VPN to openvpn

--- Applications/vpn/install-vpn.sh
+++ Applications/vpn/install-vpn.sh
@@ -5,6 +5,9 @@
 # Vergleich Openvpn und Wireguard:
 # https://www.ipvanish.com/blog/wireguard-vs-openvpn/
 
+# Open VPN mit Docker Compose:
+# https://github.com/kylemanna/docker-openvpn/blob/master/docs/docker-compose.md
+
 
 # create Applikations folder
 mkdir -p /var/homeautomation/vpn
@@ -16,4 +19,21 @@ cd /var/homeautomation/vpn
 # downlod docker-compose.yml and run it
 rm docker-compose.yml &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/vpn/docker-compose.yml &> /dev/null
 
+# Configure openvpn
+docker-compose run --rm openvpn ovpn_genconfig -u tcp://vpn.tomasz.app
+docker-compose run --rm openvpn ovpn_initpki
+
+# Start openvpn
 docker-compose up -d
+
+
+# Generate Client Certificate
+export CLIENTNAME="Test"
+# with a passphrase (recommended)
+docker-compose run --rm openvpn easyrsa build-client-full $CLIENTNAME
+# without a passphrase (not recommended)
+docker-compose run --rm openvpn easyrsa build-client-full $CLIENTNAME nopass
+
+docker-compose run --rm openvpn ovpn_getclient $CLIENTNAME > $CLIENTNAME.ovpn
+
+
