359fee7d138f23213e7153dbc44ce599428c9d7f
Belian Tomasz <bel.tomasz@gmail.com>
2022-10-09 19:29:45 +0200

    added upgrade and remove to the scripts

--- Applications/nginx/install-nginx.sh
+++ Applications/nginx/install-nginx.sh
@@ -3,14 +3,78 @@
 #cd /tmp; rm install-nginx.sh &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/nginx/install-nginx.sh &> /dev/null; bash install-nginx.sh
 
 
-# create Applikations folder
-mkdir -p /var/homeautomation/nginx
+CONTAINER_NAME=nginx
+
+install (){
+    
+    # create Applikations folder
+    mkdir -p /var/homeautomation/$CONTAINER_NAME
+    
+    
+    # change to folder
+    cd /var/homeautomation/$CONTAINER_NAME
+    
+    # downlod docker-compose.yml and run it
+    rm docker-compose.yml &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/$CONTAINER_NAME/docker-compose.yml &> /dev/null
+    
+    
+    # Start Container
+    docker-compose up -d
+    
+}
+
+# Upgrade Tools
+upgrade (){
+
+    #### ToDo
+    echo upgrade
+}
+
+uninstall (){
+    # Stop container
+    cd /var/homeautomation/$CONTAINER_NAME
+    docker-compose down
+}
+
+remove_data (){
+    # Remove Application Folder
+    rm -rv /var/homeautomation/$CONTAINER_NAME
+}
+
+# Get the options
+while getopts "urg" option; do
+    case $option in
+        u) # uninstall
+        UNINSTALL=true;;
+        r) # remove Data
+        REMOVE_DATA=true;;
+        g) # upgrade
+        UPGRADE=true;;
+        \?) # Invalid option
+            echo "Error: Invalid option"
+        exit;;
+    esac
+done
 
 
-# change to folder
-cd /var/homeautomation/nginx
+if [ $UNINSTALL ] ;then
+    # Uninstall Tools
+    uninstall
+    elif [ $UPGRADE ] ;then
+    # Upgrade Tools
+    upgrade
+else
+    if [ $REMOVE_DATA ] ;then
+        # Invalid Option
+        echo "Error: Invalid option"
+        exit 1
+    else
+        # Install Tools
+        install
+    fi
+fi
 
-# downlod docker-compose.yml and run it
-rm docker-compose.yml &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/nginx/docker-compose.yml &> /dev/null
-
-docker-compose up -d
+if [ $REMOVE_DATA ] ;then
+    # Uninstall Tools and remove data
+    remove_data
+fi
