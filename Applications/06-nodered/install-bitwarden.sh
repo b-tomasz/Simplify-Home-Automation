ae55fe28eb70ecbfd98b503f6a420fd712f0492d
Samuel Ramseyer <samuel.ramseyer@hotmail.com>
2022-10-11 11:24:23 +0200

    renaming

---
+++ Applications/06-nodered/install-bitwarden.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#Script ausführen mit:
+#cd /tmp; rm install-bitwarden.sh &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/bitwarden/install-bitwarden.sh &> /dev/null; bash install-bitwarden.sh
+
+
+CONTAINER_NAME=bitwarden
+
+install (){
+    
+    # create Applikations folder
+    mkdir -p /var/homeautomation/$CONTAINER_NAME
+    
+    
+    # change to folder
+    cd /var/homeautomation/$CONTAINER_NAME
+    
+    # downlod docker-compose.yml and run it
+    rm docker-compose.yml &> /dev/null; wget https://raw.githubusercontent.com/b-tomasz/Simplify-Home-Automation/main/Applications/$CONTAINER_NAME/docker-compose.yml &> /dev/null
+    
+    
+    # Start Container
+    docker-compose up -d
+    
+}
+
+# Upgrade Tools
+upgrade (){
+
+    #### ToDo
+    echo upgrade
+}
+
+uninstall (){
+    # Stop container
+    cd /var/homeautomation/$CONTAINER_NAME
+    docker-compose down
+}
+
+remove_data (){
+    # Remove Application Folder
+    rm -rv /var/homeautomation/$CONTAINER_NAME
+}
+
+# Get the options
+while getopts "urg" option; do
+    case $option in
+        u) # uninstall
+        UNINSTALL=true;;
+        r) # remove Data
+        REMOVE_DATA=true;;
+        g) # upgrade
+        UPGRADE=true;;
+        \?) # Invalid option
+            echo "Error: Invalid option"
+        exit;;
+    esac
+done
+
+
+if [ $UNINSTALL ] ;then
+    # Uninstall Tools
+    uninstall
+    elif [ $UPGRADE ] ;then
+    # Upgrade Tools
+    upgrade
+else
+    if [ $REMOVE_DATA ] ;then
+        # Invalid Option
+        echo "Error: Invalid option"
+        exit 1
+    else
+        # Install Tools
+        install
+    fi
+fi
+
+if [ $REMOVE_DATA ] ;then
+    # Uninstall Tools and remove data
+    remove_data
+fi
