f14389bd44f6cf5fc319714c9b8b2ccaead043d9
Samuel Ramseyer <samuel.ramseyer@hotmail.com>
2022-10-11 11:22:13 +0200

    fixes

--- Applications/nginx/install-nginx.sh
+++ Applications/nginx/install-nginx.sh
@@ -9,7 +9,47 @@ install (){
     
     # create Applikations folder
     mkdir -p /var/homeautomation/$CONTAINER_NAME
-    
+
+    # create nginx config File
+    mkdir -p /var/homeautomation/$CONTAINER_NAME/volumes/conf.d
+    cd /var/homeautomation/$CONTAINER_NAME/volumes/conf.d
+    echo #Complete Nginx Docker reverse proxy config file
+server {
+  listen 80;
+  listen [::]:80;
+  server_name localhost;
+
+  location / {
+    root /usr/share/nginx/html;
+    index index.html index.htm;
+  }
+
+  error_page 500 502 503 504 /50x.html;
+  location = /50x.html {
+    root /usr/share/nginx/html;
+  }
+}
+server {
+  listen 80;
+  server_name portainer.home;
+  location / {
+    proxy_pass https://10.0.10.1:9443;
+  }
+}
+server {
+  listen 80;
+  server_name pihole.home;
+  location / {
+    proxy_pass http://10.0.10.:8080;
+  }
+}
+server {
+  listen 80;
+  server_name grafana.example.com;
+  location / {
+    proxy_pass http://192.168.178.134:8080;
+  }
+} > homeautomation.conf
     
     # change to folder
     cd /var/homeautomation/$CONTAINER_NAME
