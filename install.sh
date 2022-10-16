eab6c2794279ca9c16493f14dbaa59e02fc7fd00
Belian Tomasz <bel.tomasz@gmail.com>
2022-10-16 12:22:52 +0200

    fix

--- install.sh
+++ install.sh
@@ -24,6 +24,12 @@
 # 2 User Exited the Script
 # 4 Script was startet as non root
 
+# Check if Script was started as Root
+if [ ! $(whoami) = root ]; then
+    whiptail --title "No Root" --msgbox "The Script needs to be Run as root.\nPlease exit the Script, then enter \"sudo su\" and rerun this Script." --ok-button "Exit" 10 78
+    exit 4
+fi
+
 # Create Script/Config/Log Folder
 mkdir -p /var/homeautomation/script/log
 mkdir -p /var/homeautomation/script/config
@@ -686,12 +692,6 @@ remove () {
 
 # Initialize Log File
 echo "Script Started at: " $(date) > $LOG_PWD/install.log
-
-# Check if Script was started as Root
-if [ ! $(whoami) = root ]; then
-    whiptail --title "No Root" --msgbox "The Script needs to be Run as root.\nPlease exit the Script, then enter \"sudo su\" and rerun this Script." --ok-button "Exit" 10 78
-    exit_script 4
-fi
 
 # Chose, what to do:
 MENU=$(whiptail --title "Install Script" --menu "What do you want to do?" --nocancel 20 78 4 \
