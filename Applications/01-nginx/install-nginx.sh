8240a426870c8a07f5ea930d7b3212a7a85bc9c2
Samuel Ramseyer <samuel.ramseyer@hotmail.com>
2022-10-19 20:06:54 +0200

    added prxyheader to all configs

--- Applications/01-nginx/install-nginx.sh
+++ Applications/01-nginx/install-nginx.sh
@@ -270,7 +270,11 @@ server {
   location / {
     proxy_pass http://10.10.30.1;
   }
+  #These header fields are required if your application is using Websockets
+	proxy_set_header Upgrade $http_upgrade;
 
+	#These header fields are required if your application is using Websockets    
+	proxy_set_header Connection "upgrade";
   ssl_certificate /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/cert.pem;
   ssl_certificate_key /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/privkey.pem;
 }
@@ -287,7 +291,11 @@ server {
   location / {
     proxy_pass http://10.10.40.1:51821;
   }
+  #These header fields are required if your application is using Websockets
+	proxy_set_header Upgrade $http_upgrade;
 
+	#These header fields are required if your application is using Websockets    
+	proxy_set_header Connection "upgrade";
   ssl_certificate /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/cert.pem;
   ssl_certificate_key /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/privkey.pem;
 }
@@ -300,7 +308,11 @@ server {
   location / {
     proxy_pass http://10.10.50.1;
   }
+  #These header fields are required if your application is using Websockets
+	proxy_set_header Upgrade $http_upgrade;
 
+	#These header fields are required if your application is using Websockets    
+	proxy_set_header Connection "upgrade";
   ssl_certificate /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/cert.pem;
   ssl_certificate_key /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/privkey.pem;
 }
@@ -340,7 +352,11 @@ server {
   location / {
     proxy_pass http://10.10.70.2:8080;
   }
+  #These header fields are required if your application is using Websockets
+	proxy_set_header Upgrade $http_upgrade;
 
+	#These header fields are required if your application is using Websockets    
+	proxy_set_header Connection "upgrade";
   ssl_certificate /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/cert.pem;
   ssl_certificate_key /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/privkey.pem;
 }
@@ -357,7 +373,11 @@ server {
   location / {
     proxy_pass http://10.10.80.1:3000;
   }
+  #These header fields are required if your application is using Websockets
+	proxy_set_header Upgrade $http_upgrade;
 
+	#These header fields are required if your application is using Websockets    
+	proxy_set_header Connection "upgrade";
   ssl_certificate /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/cert.pem;
   ssl_certificate_key /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/privkey.pem;
 }
@@ -374,7 +394,11 @@ server {
   location / {
     proxy_pass https://10.10.90.1:8443;
   }
+  #These header fields are required if your application is using Websockets
+	proxy_set_header Upgrade $http_upgrade;
 
+	#These header fields are required if your application is using Websockets    
+	proxy_set_header Connection "upgrade";
   ssl_certificate /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/cert.pem;
   ssl_certificate_key /etc/nginx/ssl/live/$EXTERNAL_DOMAIN/privkey.pem;
 }
