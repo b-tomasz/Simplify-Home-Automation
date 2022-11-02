546f1214d268057c455f74edb7423e77f7cb48ad
Belian Tomasz <bel.tomasz@gmail.com>
2022-11-02 21:26:37 +0100

    fix

--- backup.sh
+++ backup.sh
@@ -15,6 +15,9 @@ BACKUP_PWD=/mnt/backup/share
 mount -a
 sleep 2
 
+# Initialize Log File
+echo "Backup Started at: " $(date) > $LOG_PWD/backup.log
+
 if ( df | grep "$BACKUP_PWD" &> /dev/null); then
     
     echo -e "\n\n----------Start Backup----------\n" >> $LOG_PWD/backup.log
@@ -22,9 +25,15 @@ if ( df | grep "$BACKUP_PWD" &> /dev/null); then
     RUNNING_CONTAINER="$(docker ps -q)"
     
     docker container stop $RUNNING_CONTAINER
+
+    # on monday remove timestamp.dat so that the next Backup is a Full Backup
+    if [ $(date +%u) = 1 ]; then
+        rm /mnt/backup/share/timestamp.dat &>> $LOG_PWD/backup.log
+    fi
     
     # Backup Data:
-    tar -cvpzf /mnt/backup/share/level0.tar.gz -g /mnt/backup/share/timestamp.dat /var/homeautomation/
+    mkdir -p /mnt/backup/share/backup
+    tar -cvpzf /mnt/backup/share/backup/$(date +%Y-%m-%d-%H%M%S).tar.gz -g /mnt/backup/share/timestamp.dat /var/homeautomation/ &>> $LOG_PWD/backup.log
     
     
     
@@ -36,4 +45,3 @@ else
     echo -e "Backup Path not found" >> $LOG_PWD/backup.log
 fi
 
-WORKSPACE_NAME
